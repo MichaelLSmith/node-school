a51c20fe0b3d379521f44eeaf89b37c0473b9765
michaelLsmith <mlsmith45@gmail.com>
2017-06-16 16:10:07 -0400

    got application-no-bind

--- application-no-bind.js
+++ application-no-bind.js
@@ -2,6 +2,13 @@ var slice = Array.prototype.slice
 
 function logger(namespace) {
 
+  function inner() {
+    //arguments of inner will be what is passed to info()
+    //so we need to pass the arguments of inner to the apply call of console
+    var args = slice.call(arguments);
+    return console.log.apply(null, [namespace].concat(args));
+  }
+  return inner;
 }
 
 module.exports = logger
--- dump.js
+++ dump.js
@@ -5,12 +5,25 @@ function add(x,y) {
 }
 
 function partiallyApply(fn, z) {
-  return function(t) {
-    fnArgs = [z,t]
-    return fn.apply(null, fnArgs);
+  return function(t) {//this is what addTen() looks like. it refers to this function with add()'s first arg predined as 10'. t becomes y in add().
+    //in add():
+      //t is y
+      //z is x
+    return fn.apply(null, [z,t]);
   }
 }
 
 var addTen = partiallyApply(add,10);
 
 console.log('addTen(20):', addTen(40));
+
+
+
+
+var info = logger('INFO:')
+info('this is an info message')
+// INFO: this is an info message
+
+var warn = logger('WARN:')
+warn('this is a warning message', 'with more info')
+// WARN: this is a warning message with more info
